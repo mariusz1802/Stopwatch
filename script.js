454aae2ad6bc8d6de5d0a5e3b4a871c42a4daeb3
Mariusz <mariusz.janowski@yahoo.pl>
2023-01-20 18:15:49 +0100

    init projekct

--- script.js
+++ script.js
@@ -5,67 +5,22 @@ class Stopwatch {
     this.milisecond = milisecond;
   }
 
-
-   start() {
-    console.log("start");
-    tens++;
-    if(tens <= 9){
-      milisecond.innerHTML = "0" + tens;
-    }
-    if(tens > 9){
-      milisecond.innerHTML = tens;
-    }
-  
-    if( tens > 99){
-        seconds++
-        if(seconds <= 9) {
-          second.innerHTML = "0" + seconds;
-          tens = 0;
-          milisecond.innerHTML = "0" +0;
-        }
-        if(seconds > 9){
-          second.innerHTML = seconds;
-        }
-    }
-    if(seconds > 59){
-        minutes++
-        if(minutes <= 9) {
-              minute.innerHTML = "0" + minutes
-              seconds = 0;
-              second.innerHTML = "0" + 0; 
-
-        }
-
-        minute.innerHTML = minutes;
-        if(minute > 59) {
-            this.reset();
-        }
-    }
+  start() {
+    console.log(milisecond++);
   }
-
-  stop() {
-    alert("stop");
-  }
+  stop() {}
   reset() {
     alert("Reset");
   }
-
-
 }
-let minutes = 00;
-let seconds = 00;
-let tens = 00;
+let minutes = 0;
+let seconds = 0;
+let tens = 0;
 let Interval;
 
-
-
-
-
-
-
-let minute = document.querySelector(".minute");
-let second = document.querySelector(".second");
-let milisecond = document.querySelector(".milisecond");
+let minute = document.querySelector(".minute").innerText;
+let second = document.querySelector(".second").innerText;
+let milisecond = document.querySelector(".milisecond").innerText;
 
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
@@ -74,18 +29,13 @@ const resettBtn = document.querySelector("#resetBtn");
 const stopwatch = new Stopwatch(minute, second, milisecond);
 
 startBtn.addEventListener("click", () => {
-  clearInterval(Interval);
-  Interval = setInterval(stopwatch.start, 1)
- 
+  stopwatch.start();
 });
 
 stopBtn.addEventListener("click", () => {
   stopwatch.stop();
- 
 });
 
 resettBtn.addEventListener("click", () => {
   stopwatch.reset();
-
 });
-
